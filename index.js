5c2d54054f700f797415c2f724c86c56959b0b79
NachiketProxy <166910327+NachiketProxy@users.noreply.github.com>
2024-08-16 00:38:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,26 @@
 import express from "express";
-
+import path from "path";
 
 const app = express();
-const port = 3000;
 
-app.use(express.static("public"));
-
+// Configure EJS and static file serving
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
+// Define routes
+app.get("/", (req, res) => res.render("about"));
+app.get("/login", (req, res) => res.render("login"));
+app.get("/adminlogin", (req, res) => res.render("adminlogin"));
+app.get("/teacherlogin", (req, res) => res.render("teacherlogin"));
+app.get("/adminDashboard", (req, res) => res.render("adminDashboard"));
+app.get("/teacherDashboard", (req, res) => res.render("teacherDashboard"));
 
-app.get("/", (req, res) => {
-    res.render("about.ejs");
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
-app.get("/login", (req, res) => {
-    res.render("login.ejs");
-});
-
-app.get("/adminlogin", (req, res) => {
-    res.render("adminlogin.ejs");
-});
-
-app.get("/teacherlogin", (req, res) => {
-    res.render("teacherlogin.ejs");
-});
-
-app.get("/adminDashboard", (req, res) => {
-    res.render("adminDashboard.ejs");
-});
-
-app.get("/teacherDashboard", (req, res) => {
-    res.render("teacherDashboard.ejs");
-});
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-})
+// Export the app as a serverless function
+export default (req, res) => app(req, res);
